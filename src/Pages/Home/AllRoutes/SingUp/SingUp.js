67f0a68d204d7aeec5db6cd02c51ee5cfb753e27
suzitsarkar27 <96899998+suzitsarkar27@users.noreply.github.com>
2022-05-22 16:29:59 +0600

    add-banner

--- src/Pages/Home/AllRoutes/SingUp/SingUp.js
+++ src/Pages/Home/AllRoutes/SingUp/SingUp.js
@@ -2,18 +2,41 @@ import React, { useRef } from 'react';
 import { useCreateUserWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
 import auth from '../../../SHAREit/Firebase/firebase.init';
+import Loading from '../../../SHAREit/Loading/Loading';
 
 const SingUp = () => {
-   
-    const namelRef = useRef("");
     const emailRef = useRef("");
     const passwordRef = useRef("");
     const repasswordRef = useRef("");
     const navigate = useNavigate();
+
+
     const [createUserWithEmailAndPassword, user, loading, error] =
-    useCreateUserWithEmailAndPassword(auth);
+    useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
     const [signInWithGoogle] = useSignInWithGoogle(auth);
 
+    let errorElement;
+     
+    if (user) {
+        navigate("/home");
+      }
+
+
+      if (user) {
+        console.log("user", user);
+      }
+
+      if(loading){
+          return <Loading></Loading>
+      }
+
+      if (error) {
+        errorElement = <p>Error:{error?.message}</p>;
+      }
+    
+      if (error) {
+        errorElement = <p className="text-danger">Error: {error?.message}</p>;
+      }
 
     const handelSubmit = async (e) => {
         e.preventDefault();
@@ -21,12 +44,16 @@ const SingUp = () => {
         const password = passwordRef.current.value;
         const repassword = repasswordRef.current.value;
         await createUserWithEmailAndPassword(email, password, repassword);
+        navigate("/home");
       };
 
     return (
         <div className="flex justify-center items-center h-screen">
         <div className="hero min-h-screen ">
-          <div className="hero-content flex-col lg:flex-row-reverse  w-96">
+         <div className="hero-content flex-col  w-96">
+            <div class="text-center ">
+            <h1 class="text-4xl font-bold text-secondary">Reagister Now !</h1>
+            </div>
             <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
               <form onSubmit={handelSubmit} className="card-body">
 
@@ -34,26 +61,26 @@ const SingUp = () => {
                   <label className="label">
                     <span className="label-text">Email</span>
                   </label>
+
                   <input
                     type="email"
                     placeholder="Email"
                     ref={emailRef}
-                    className="input input-bordered"
-                    required
-                  />
+                    className="input input-bordered" required />
                 </div>
+
                 <div className="form-control">
                   <label className="label">
                     <span className="label-text">Password</span>
                   </label>
+
                   <input
                     type="password"
                     placeholder="Password"
                     ref={passwordRef}
-                    className="input input-bordered"
-                    required
-                  />
+                    className="input input-bordered" required />
                 </div>
+
                 <div className="form-control">
                   <label className="label">
                     <span className="label-text">Re-Password</span>
@@ -62,25 +89,19 @@ const SingUp = () => {
                     type="password"
                     ref={repasswordRef}
                     placeholder="Re-Password"
-                    className="input input-bordered"
-                    required
-                  />
+                    className="input input-bordered" required />
                 </div>
+                     {errorElement}
                 <div className="form-control mt-6">
                  <input type="submit" value={'Login'}className="btn btn-primary text-white" />
                 </div>
-                <p>
-                  Allready have accound :{" "}
-                  <Link to={"/login"} className="text-secondary font-bold">
-                    Login Now
-                  </Link>
-                </p>
+
+                <p>  Allready have accound : <Link to={"/login"} className="text-secondary font-bold"> Login Now </Link> </p>
                 <div className="flex flex-col w-full border-opacity-50">
                   <div className="divider">OR</div>
                   <input
                     type="submit"
-                    onClick={() => signInWithGoogle()}
-                    value={"Sing In Google"}
+                    onClick={() => signInWithGoogle()} value={"Sing In Google"}
                     className="input input-bordered btn btn-primary text-white"
                   />
                 </div>
